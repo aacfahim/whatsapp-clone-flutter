447b7c768bcc9c8890da19e17d01f1cb7afaba4a
aacfahim <aacfahim@gmail.com>
2022-12-06 05:44:17 +0600

    widget tree updated

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -12,43 +12,41 @@ class ContactsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 10.0),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: info.length,
-                itemBuilder: ((context, index) {
-                  return InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: ListTile(
-                        title: Text("${info[index]['name']}",
-                            style: TextStyle(fontSize: 18)),
-                        subtitle: Padding(
-                          padding: const EdgeInsets.only(top: 4.0),
-                          child: Text("${info[index]['message']}",
-                              style: TextStyle(fontSize: 15)),
-                        ),
-                        leading: CircleAvatar(
-                          radius: 25,
-                          backgroundImage:
-                              NetworkImage("${info[index]['profilePic']}"),
-                        ),
-                        trailing: Text("${info[index]['time']}",
-                            style: TextStyle(fontSize: 13, color: Colors.grey)),
+      child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: info.length,
+          itemBuilder: ((context, index) {
+            return Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: ListTile(
+                      title: Text("${info[index]['name']}",
+                          style: TextStyle(fontSize: 18)),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 4.0),
+                        child: Text("${info[index]['message']}",
+                            style: TextStyle(fontSize: 15)),
                       ),
+                      leading: CircleAvatar(
+                        radius: 25,
+                        backgroundImage:
+                            NetworkImage("${info[index]['profilePic']}"),
+                      ),
+                      trailing: Text("${info[index]['time']}",
+                          style: TextStyle(fontSize: 13, color: Colors.grey)),
                     ),
-                  );
-                })),
-          ),
-          Divider(
-            color: dividerColor,
-            indent: 85,
-          ),
-        ],
-      ),
+                  ),
+                ),
+                Divider(
+                  color: dividerColor,
+                  indent: 85,
+                ),
+              ],
+            );
+          })),
     );
   }
 }
