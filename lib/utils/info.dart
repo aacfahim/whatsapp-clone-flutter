5e56134f0a153c4b3bf2fbd0dd30fe4764aa80dc
aacfahim <aacfahim@gmail.com>
2022-12-08 05:08:56 +0600

    dummy data

--- lib/utils/info.dart
+++ lib/utils/info.dart
@@ -77,46 +77,43 @@ const messages = [
   {"isMe": false, "text": "I am great man!", "time": "11:01 am"},
   {
     "isMe": false,
-    "text": "Just messaged cuz I had some work.",
+    "text": "Nothing much... just messaged for a silly thing.",
     "time": "11:01 am"
   },
   {"isMe": true, "text": "Obviously, say", "time": "11:03 am"},
   {
     "isMe": false,
-    "text": "haha I wanted you to check out my new channel ^^",
+    "text": "haha I wanted you to check out github profile ^^",
     "time": "11:04 am"
   },
-  {
-    "isMe": true,
-    "text": " Sure, what is the channel name?",
-    "time": "11:05 am"
-  },
+  {"isMe": true, "text": " Sure, what is the github link?", "time": "11:05 am"},
   {
     "isMe": false,
-    "text": "Rivaan Ranawat",
+    "text": "aacfahim",
     "time": "11:06 am",
   },
   {
     "isMe": true,
-    "text": "Looks great to me!",
+    "text":
+        "Looks great to me, i really liked the instagram clone app source code!",
     "time": "11:15 am",
   },
   {"isMe": false, "text": "Thanks bro!", "time": "11:17 am"},
-  {"isMe": false, "text": "Did you subscribe?", "time": "11:16 am"},
+  {"isMe": false, "text": "Did you follow me 😝?", "time": "11:16 am"},
   {"isMe": true, "text": "Yes, surely bro!", "time": "11:17 am"},
   {
     "isMe": false,
-    "text": "Cool, did you like the content?",
+    "text": "Cool, did you like the other repos?",
     "time": "11:18 am",
   },
   {
     "isMe": true,
-    "text": "I loved it?",
+    "text": "I loved it",
     "time": "11:19 am",
   },
   {
     "isMe": false,
-    "text": "OMG! Woah! Thanks!",
+    "text": "xD Woah! Bro!",
     "time": "11:20 am",
   },
 ];
